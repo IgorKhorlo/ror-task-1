9d839b21a5d597e5f54c7536e12d4db59afe86cd
Igor Khorlo <sspkmnd@gmail.com>
2015-07-13 12:32:13 +0000

    final changes + rspec

--- ror_task_1.rb
+++ ror_task_1.rb
@@ -1,10 +1,12 @@
+require 'pry'
+
 # http://edx.raccoongang.com/courses/IT-People/RoR101/spring2015/courseware/b970871802734615acb83348a295f883/835bd3a65e484ed08b8ebd4cd2612490/
 class RorTask1
   # Напишите программу, которая для заданного натурального числа num определяет
   # количество вхождений числа 3 в разложение заданного числа на простые сомножители.
   def how_many_threes(num)
     result = 0
-    while num % 3 == 0
+    while num > 0 && num % 3 == 0
       num /= 3
       result += 1
     end
@@ -13,7 +15,7 @@ class RorTask1
 
   # Дан текст. Определите максимальное из имеющихся в нем чисел.
   def max_number_in_text(text)
-    text.scan(/\d([.]\d+|)+/).map(&:to_f).max
+    text.scan(/\d+(?:[.]\d+|)/).map(&:to_f).max
   end
 
   # Дан целочисленный массив. Необходимо вывести вначале его элементы с четными
@@ -36,7 +38,7 @@ class RorTask1
     result = []
     result << arr.first
     1.upto(arr.size - 2) do |i|
-      result << arr[i]
+      result << ((arr[i].odd?) ? arr[i] + arr.last : arr[i])
     end
     result << arr.last
   end
@@ -45,7 +47,7 @@ class RorTask1
   # из данного массива, расположенных между первыми двумя нулями.
   def sum_between_zeros(arr)
     first_index = arr.index(0) + 1
-    second_index = arr[first_index..-1].index(0) - 1
+    second_index = arr[first_index..-1].index(0) + first_index - 1
     arr[first_index..second_index].inject(:+)
   end
 end
--- ror_task_1_rspec.rb
+++ ror_task_1_rspec.rb
@@ -4,21 +4,21 @@ describe RorTask1 do
   let(:task) { RorTask1.new }
 
   it 'Should return multiplicity of 3' do
-    expect(task.how_many_threes 225). to eq 2
-    expect(task.how_many_threes 27). to eq 3
-    expect(task.how_many_threes 0). to eq 0
+    expect(task.how_many_threes 225).to eq 2
+    expect(task.how_many_threes 27).to eq 3
+    expect(task.how_many_threes 0).to eq 0
   end
 
   it 'Should return max number in text' do
-    expect(task.max_number_in_text 'Text with99 numbers 5 3 OK and floats 13.4 404.404'). to eq 404.404
+    expect(task.max_number_in_text 'Text with99 numbers 5 3 OK and floats 13.4 404.404').to eq 404.404
   end
 
   it 'Should return elements at even indexes then at odd as Array' do
-    expect(task.even_odd_elements [0.1, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1]). to eq []
+    expect(task.even_odd_elements [0.1, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1, 8.1]).to eq [0.1, 2.1, 4.1, 6.1, 8.1, 1.1, 3.1, 5.1, 7.1]
   end
 
   it 'Should do some actions' do
-    except(task.even_elements_addition [1, 2, 3, 4, 5, 1]). to eq []
+    expect(task.even_elements_addition [1, 2, 3, 4, 5, 1]).to eq [1, 2, 4, 4, 6, 1]
   end
 
   it 'Should return sum between zeros' do
